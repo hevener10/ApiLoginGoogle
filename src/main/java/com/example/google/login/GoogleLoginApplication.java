9bfed6e62a0a1fd40d79fcd0193eec4057d1ce09
Hevener Ancelmo Pereira <hevener2@hotmail.com>
2023-03-24 18:31:31 -0300

    Adicionando swagger simples

--- src/main/java/com/example/google/login/GoogleLoginApplication.java
+++ src/main/java/com/example/google/login/GoogleLoginApplication.java
@@ -3,6 +3,8 @@ package com.example.google.login;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
+
 @SpringBootApplication
 public class GoogleLoginApplication {
 
